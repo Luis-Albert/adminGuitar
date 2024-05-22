4cfcf08b3764f0b2187fb91377aefb8154b3f2fb
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-05-21 21:42:53 -0500

    Agregando el main y el footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ function App() {
   return (
     <>
       <Header />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Coleccion</h2>
+        <div className="row mt-5">
+
+        </div>
+      </main>
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+      </footer>
     </>
   )
 }
