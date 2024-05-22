610e96da00cb71f0c0c7a3e81b554292c471fe64
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-05-21 21:38:54 -0500

    Agregando el boton de carrito compras

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,22 @@ function Header() {
           <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
             <div className="carrito">
               <img src="/img/carrito.png" alt="imagen carrito" className="img-fluid" />
-              <div className="bg-white p-3" id="carrito"></div>
+              <div className="bg-white p-3" id="carrito">
+                <table className="w-100 table">
+                  <thead>
+                    <tr>
+                      <th>Imagen</th>
+                      <th>Nombre</th>
+                      <th>Precio</th>
+                      <th>Cantidad</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                </table>
+                <button className="btn btn-dark w-100 mt-3 p-2">
+                  Vaciar carrito
+                </button>
+              </div>
             </div>
           </nav>
         </div>
